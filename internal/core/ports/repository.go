a92cec524ac649aeb892c2bc4d2e6d80ee587bba
xpan <xpanvictor@gmail.com>
2025-01-05 01:30:08 +0100

    chore(cleanup): clears a few messes

--- internal/core/ports/repository.go
+++
@@ -1,9 +0,0 @@
-package ports
-
-import "go_graphql_gin/internal/core/domain"
-
-type UserRepository interface {
-	FindByID(id string) (*domain.User, error)
-	Create(user *domain.User) error
-	// Other methods...
-}
